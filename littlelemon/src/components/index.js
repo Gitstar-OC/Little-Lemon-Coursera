419cf08bd7f5d9e88a8e54f58f67b454be8b353c
Gitstar-Om <chandankarom07@gmail.com>
2024-04-21 12:31:13 +0530

    Modified index.js

--- littlelemon/src/components/index.js
+++ littlelemon/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Testimonials } from './Testimonials/Testimonials';
-export { default as AboutLittleLemon } from './AboutLittleLemon/AboutLittleLemon';
-export { default as HighLightsAndSpecials } from './HighLightsAndSpecials/HighLightsAndSpecials';
+export { default as About } from './About/About';
+export { default as HighLights } from './HighLights/HighLights';
 export { default as Footer } from './Footer/Footer';
