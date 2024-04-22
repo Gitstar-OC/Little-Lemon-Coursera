02c688a2ed694c4f98c31aba211bbefab372a726
Gitstar-Om <chandankarom07@gmail.com>
2024-04-23 00:15:53 +0530

    Added index.js (Exports file) in Container

---
+++ littlelemon/src/container/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home/Home';
+export { default as Login } from './Login/Login';
+export { default as Menu } from './Menu/Menu';
+export { default as OrderOnline } from './OrderOnline/OrderOnline';
+export { default as Reservations } from './Reservations/Reservations';
