8c299c3a161ed99387731dd155ac5597907b779f
Gitstar-Om <chandankarom07@gmail.com>
2024-04-22 23:57:19 +0530

    Added things in Navbar

--- littlelemon/src/components/Navbar/Navbar.jsx
+++ littlelemon/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import "./Navbar.css"
 import image from '../../Assets/littlelemon2.png'
+import { Routes, Route, Link } from'react-router-dom'
+
+import {About} from "./components"
 
 
 const Navbar = () => {
   return (
     <nav>
        <img style={{width: '233px', height: '79px'}} src={image} />
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/orderOnline" element={<OrderOnline  />} />
+          <Route path="/log-in" element={<LogIn />} />
+        </Routes>
+        <Link to="/" className="nav-item">Home</Link>
+        <Link to="/about" className="nav-item">About</Link>
+        <Link to="/menu" className="nav-item">Menu</Link>
+        <Link to="/reservations" className="nav-item">Reservations</Link>
+        <Link to="/orderOnline" className="nav-item">Order Online</Link>
+        <Link to="/log-in" className="nav-item">Log In</Link>
+
     </nav>
   )
 }
