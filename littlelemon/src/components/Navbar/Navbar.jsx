f059badc1ee3e9052d7030bc9b80a1634d736ad0
Gitstar-Om <chandankarom07@gmail.com>
2024-04-23 21:15:24 +0530

    Error resolved, app started working smoothly

--- littlelemon/src/components/Navbar/Navbar.jsx
+++ littlelemon/src/components/Navbar/Navbar.jsx
@@ -3,22 +3,23 @@ import "./Navbar.css";
 import image from '../../Assets/littlelemon2.png';
 import { Routes, Route, Link } from 'react-router-dom';
 import { About } from "../index";
-import { LogIn, OrderOnline, Menu, Reservations } from '../../container/index';
+import { LogIn, OrderOnline, Menu, Reservations, Home  } from '../../container/index';
 // import App from "../../App";
+
 
 const Navbar = () => {
   return (
     <nav>
       <img src={image} alt="Logo of little lemon" />
        <Routes>
-        {/* <Route path="/" element={<App />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/reservations" element={<Reservations />} />
         <Route path="/orderOnline" element={<OrderOnline />} />
         <Route path="/log-in" element={<LogIn />} />
       </Routes>
-      {/* <Link to="/" className="nav-item">Home</Link> */}
+      <Link to="/" className="nav-item">Home</Link>
       <Link to="/about" className="nav-item">About</Link>
       <Link to="/menu" className="nav-item">Menu</Link>
       <Link to="/reservations" className="nav-item">Reservations</Link>
