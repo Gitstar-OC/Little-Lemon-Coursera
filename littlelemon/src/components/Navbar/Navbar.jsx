d53c79c0a893b3b4005b31a0a88ef6682c639620
Gitstar-Om <chandankarom07@gmail.com>
2024-04-23 21:12:33 +0530

    Added home page and will change the navbar app import to home (The app import was causing error and looping through the whole process which created ran out of memory error)

--- littlelemon/src/components/Navbar/Navbar.jsx
+++ littlelemon/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import "./Navbar.css";
 import image from '../../Assets/littlelemon2.png';
-// import { Routes, Route, Link } from 'react-router-dom';
-// import { About } from "../index";
-// import { LogIn, OrderOnline, Menu, Reservations } from '../../container/index';
+import { Routes, Route, Link } from 'react-router-dom';
+import { About } from "../index";
+import { LogIn, OrderOnline, Menu, Reservations } from '../../container/index';
 // import App from "../../App";
 
 const Navbar = () => {
   return (
     <nav>
       <img src={image} alt="Logo of little lemon" />
-      {/* <Routes>
-        <Route path="/" element={<App />} />
+       <Routes>
+        {/* <Route path="/" element={<App />} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/reservations" element={<Reservations />} />
         <Route path="/orderOnline" element={<OrderOnline />} />
         <Route path="/log-in" element={<LogIn />} />
       </Routes>
-      <Link to="/" className="nav-item">Home</Link>
+      {/* <Link to="/" className="nav-item">Home</Link> */}
       <Link to="/about" className="nav-item">About</Link>
       <Link to="/menu" className="nav-item">Menu</Link>
       <Link to="/reservations" className="nav-item">Reservations</Link>
       <Link to="/orderOnline" className="nav-item">Order Online</Link>
-      <Link to="/log-in" className="nav-item">Log In</Link> */}
+      <Link to="/log-in" className="nav-item">Log In</Link> 
     </nav>  
   )
 }
