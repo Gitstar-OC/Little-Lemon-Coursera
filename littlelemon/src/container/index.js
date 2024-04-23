e405e16dc1107a6b4863c6e0aaaf2a0cbe1c1e70
Gitstar-Om <chandankarom07@gmail.com>
2024-04-23 21:13:35 +0530

    updated exports

--- littlelemon/src/container/index.js
+++ littlelemon/src/container/index.js
@@ -2,3 +2,4 @@ export { default as LogIn } from './LogIn/LogIn';
 export { default as Menu } from './Menu/Menu';
 export { default as OrderOnline } from './OrderOnline/OrderOnline';
 export { default as Reservations } from './Reservations/Reservations';
+export { default as Home} from './Home/Home';
