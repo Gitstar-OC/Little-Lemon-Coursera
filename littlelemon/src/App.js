49ff19da3d7d930887cd93a983a8448706e82ecb
Gitstar-Om <chandankarom07@gmail.com>
2024-04-23 11:29:56 +0530

    Removed home and replaced it with app.js

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
 import { About, Footer, Hero, HighLights, Navbar, Testimonials } from './components';
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+// import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
 
-function AppRoutes() {
-  let routes = useRoutes([
-    { path: "/home", element: <Home /> },
-    { path: "/login", element: <LogIn /> },
-    // ... other routes
-  ]);
-  return <>{routes}</>;
-}
+
+// function AppRoutes() {
+//   let routes = useRoutes([
+//     { path: "/home", element: <Home /> },
+//     { path: "/login", element: <LogIn /> },
+
+//   ]);
+//   return <>{routes}</>;
+// }
 
 function App() {
   return (
-    <Router>
+      <>
       <Navbar />
-      <AppRoutes />
       <Hero />
       <HighLights />
       <Testimonials />
       <About />
       <Footer />
-    </Router>
+      </>
   );
 }
 
--- littlelemon/src/container/index.js
+++ littlelemon/src/container/index.js
@@ -1,4 +1,3 @@
-export { default as Home } from './Home/Home';
 export { default as LogIn } from './LogIn/LogIn';
 export { default as Menu } from './Menu/Menu';
 export { default as OrderOnline } from './OrderOnline/OrderOnline';
