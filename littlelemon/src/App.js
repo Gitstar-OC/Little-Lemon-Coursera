70acb0ddc7d63118ce28472e17a48e1ba54866a0
Gitstar-Om <chandankarom07@gmail.com>
2024-04-23 11:11:25 +0530

    Added Browser router in App.js files (Some errors remain to be fixed)

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-
 import { About, Footer, Hero, HighLights, Navbar, Testimonials } from './components';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+
+function AppRoutes() {
+  let routes = useRoutes([
+    { path: "/home", element: <Home /> },
+    { path: "/login", element: <LogIn /> },
+    // ... other routes
+  ]);
+  return <>{routes}</>;
+}
 
 function App() {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <HighLights />
-            <Testimonials />
-            <About />
-            <Footer />
-        </>
-    )
+  return (
+    <Router>
+      <Navbar />
+      <AppRoutes />
+      <Hero />
+      <HighLights />
+      <Testimonials />
+      <About />
+      <Footer />
+    </Router>
+  );
 }
+
 
 export default App;
