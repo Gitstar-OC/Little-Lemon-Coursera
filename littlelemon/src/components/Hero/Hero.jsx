40c7133e7a43b0412c3c4cf4efb1fa7f2da6b8ff
Gitstar-Om <chandankarom07@gmail.com>
2024-04-24 14:30:50 +0530

    Working on hero section (CTA):

--- littlelemon/src/components/Hero/Hero.jsx
+++ littlelemon/src/components/Hero/Hero.jsx
@@ -3,9 +3,11 @@ import "./Hero.css"
 
 const Hero = () => {
   return (
-    <>
-      
-    </>
+    <main>
+      <h1> Little Lemon </h1>
+      <h2> Chicago </h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </main>
   )
 }
 
