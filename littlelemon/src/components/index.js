66e97d2590989d79a7365a19be48ae1d71cf1ddc
Gitstar-OC <chandankarom07@gmail.com>
2024-05-11 12:11:51 +0530

    Recreated whole app with clear instructions

--- littlelemon/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Testimonials } from './Testimonials/Testimonials';
-export { default as HighLights } from './HighLights/HighLights';
-export { default as Footer } from './Footer/Footer';
-export { default as Hero } from './Hero/Hero';
--- littlelemon/src/container/index.js
+++
@@ -1,6 +0,0 @@
-export { default as LogIn } from './LogIn/LogIn';
-export { default as Menu } from './Menu/Menu';
-export { default as OrderOnline } from './OrderOnline/OrderOnline';
-export { default as Reservations } from './Reservations/Reservations';
-export { default as Home} from './Home/Home';
-export { default as About } from './About/About';
