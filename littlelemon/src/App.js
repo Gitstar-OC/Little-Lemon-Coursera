aa9ddff808a8858f32ea122e7e5f3fc8bea87a8f
Gitstar-OC <chandankarom07@gmail.com>
2024-05-11 12:04:38 +0530

    Added final app.js file

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,26 +1,47 @@
-// import './App.css';
-import React from 'react'
-import { Home, About, LogIn, OrderOnline, Menu, Reservations } from './container/index';
-import {Navbar} from "./components/index"
-import { Routes, Route, } from 'react-router-dom';
+import {
+  Route, 
+  Routes
+} from 'react-router-dom';
+import pages from './utils/pages';
+import Layout from './components/layout/Layout';
+import Home from './components/pages/Home';
+import Bookings from './components/pages/Bookings';
+import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
+import NotFound from './components/pages/NotFound';
+import UnderConstruction from './components/pages/UnderConstruction';
 
-
- function App() {
+const App = () => {
   return (
-      <>
-
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/orderOnline" element={<OrderOnline />} />
-        <Route path="/log-in" element={<LogIn />} />
-      </Routes>
-      </>
+    <>
+      <Layout>
+        <Routes>
+          <Route path={pages.get('home').path} element={<Home />} />
+          <Route 
+            path={pages.get('about').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('menu').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+          <Route 
+            path={pages.get('confirmedBooking').path} 
+            element={<ConfirmedBooking />} 
+          />
+          <Route 
+            path={pages.get('orderOnline').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('login').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </>
   );
-}
+};
 
-
- export default App;
+export default App;
