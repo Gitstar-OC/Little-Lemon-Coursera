1db75e24cced3a2cdc4f8c952e65ef607a53c298
Gitstar-OC <chandankarom07@gmail.com>
2024-05-11 16:45:25 +0530

    updated the file app is now working, now I will do a little bit of changes and project is complete

--- littlelemon/src/index.js
+++ littlelemon/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'modern-normalize/modern-normalize.css';
+// import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './App';
 import ScrollToTop from './components/layout/ScrollToTop';
