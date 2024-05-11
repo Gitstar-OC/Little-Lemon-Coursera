e61121a052189a4c08ad130e2b3cf5fe4c0f75f8
Gitstar-OC <chandankarom07@gmail.com>
2024-05-11 16:38:48 +0530

    Added some icons in folder and modified header.js

--- littlelemon/src/components/layout/Header/Header.js
+++ littlelemon/src/components/layout/Header/Header.js
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import logoImage from './assets/logo.png';
+import logoImage from './../assets/logo.png';
 import './Header.css';
-import pages from '../../utils/pages';
+import pages from './../../../utils/pages';
 
 const navLinks = Array.from(pages.values()).filter(page => page.anchorable);
 
@@ -18,23 +18,23 @@ const Header = () => {
         <Link className="nav-bar-logo" to={pages.get('home').path}>
           <img src={logoImage} alt="Little Lemon logo" />
         </Link>
-        <button 
-          className="nav-bar-hamburger" 
-          type="button" 
+        <button
+          className="nav-bar-hamburger"
+          type="button"
           onClick={() => setIsNavExpanded(!isNavExpanded)}
         >
           {isNavExpanded ?
-            <FontAwesomeIcon icon={faXmark} size="2x" /> : 
+            <FontAwesomeIcon icon={faXmark} size="2x" /> :
             <FontAwesomeIcon icon={faBars} size="2x" />}
         </button>
-        <ul 
-          className={isNavExpanded ? 'nav-bar-links expanded' : 'nav-bar-links'} 
+        <ul
+          className={isNavExpanded ? 'nav-bar-links expanded' : 'nav-bar-links'}
           onClick={() => setIsNavExpanded(!isNavExpanded)}
         >
-          {navLinks.map((navLink, index) => 
+          {navLinks.map((navLink, index) =>
             <li key={index}>
-              <Link 
-                className={pathname === navLink.path ? 'current-location' : ''} 
+              <Link
+                className={pathname === navLink.path ? 'current-location' : ''}
                 to={navLink.path}
               >
                 {navLink.name}
